ec8d728700e93d4702a6d0b6f2d81f35c1aa8e69
mbsantiago <santiago.mbal@gmail.com>
2019-11-30 15:48:55 -0600

    added full functionality to create thumbnail endpoint

--- selia_thumbnails/views/thumbnails.py
+++ selia_thumbnails/views/thumbnails.py
@@ -10,20 +10,40 @@ from irekua_permissions.items import items as permissions
 @require_GET
 def generate_thumbnail(request):
     item = Item.objects.get(pk=request.GET["pk"])
-    size = '100x100'
-    crop = 'center'
-    quality = 99
+    geometry = '100x100'
 
     if 'size' in request.GET:
-        size = request.GET["size"]
-    if 'quality' in request.GET:
-        quality = int(request.GET["quality"])
-    if 'crop' in request.GET:
-        crop = request.GET["crop"]
+        geometry = request.GET["size"]
+    elif 'width' in request.GET:
+        geometry = request.GET['width']
+    elif 'height' in request.GET:
+        geometry = request.GET['height']
+
+    crop = request.GET.get("crop", 'center')
+    upscale = request.GET.get("upscale", True)
+    quality = request.GET.get("quality", 95)
+    progressive = request.GET.get("progressive", True)
+    orientation = request.GET.get("orientation", True)
+    format = request.GET.get("format", 'JPEG')
+    colorspace = request.GET.get("colorspace", 'RGB')
+    padding = request.GET.get("padding", True)
+    padding_color = request.GET.get("padding_color", '#ffffff')
 
     try:
-        image = get_thumbnail(item.item_thumbnail, size, crop=crop, quality=quality)
+        image = get_thumbnail(
+            item.item_thumbnail,
+            geometry,
+            crop=crop,
+            quality=quality,
+            upscale=upscale,
+            progressive=progressive,
+            orientation=orientation,
+            format=format,
+            colorspace=colorspace,
+            padding=padding,
+            padding_color=padding_color)
     except Exception as error:
+        print(error)
         return HttpResponse(status=500)
 
     return redirect(image.url)
